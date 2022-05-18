c3a5d5b35049ad8e0466a32956eeed4370d6babd
painterpuppets <painterpuppets@gmail.com>
2022-05-19 02:12:18 +0800

    add username verify

--- src/helper.ts
+++ src/helper.ts
@@ -1,5 +1,28 @@
 import NodeRSA from 'node-rsa';
 import { UPAuthResponse } from './interface';
+import { utils, HexString } from '@ckb-lumos/lumos';
+import axios from 'axios'
+import { createHash } from 'crypto'
+const AGGREGATOR_URL = process.env.AGGREGATOR_URL || 'https://t.aggregator.unipass.id/dev/'
+
+interface RSAPubkey {
+  e: number
+  n: string
+}
+
+interface Pubkey {
+  secp256k1?: string
+  rsa_pubkey?: RSAPubkey
+}
+
+interface UserInfo {
+  username: string
+  local_keys: Pubkey[]
+}
+
+export function sha256(message: string) {
+  return `0x${createHash('SHA256').update(Buffer.from(message)).digest('hex')}`;
+}
 
 export function getPubkeyFromUPKey(upPubkey: string) {
   const key = new NodeRSA()
@@ -21,16 +44,24 @@ export function getPubkeyFromUPKey(upPubkey: string) {
 }
 
 export async function verifyUniPassSig(
+  username: string,
   msg: string,
   authResp: UPAuthResponse,
 ): Promise<boolean> {
   const { keyType, pubkey, sig } = authResp
+
+
+  const isPubkeyOnChain = await checkPubkeyOnChain(username, keyType, pubkey)
+  if (!isPubkeyOnChain) {
+    throw new Error(`pubkey not valid on chain`)
+  }
+
   if (keyType !== 'RsaPubkey') {
     throw new Error(`UnSupported keyType ${keyType}`)
   }
 
   const key = getPubkeyFromUPKey(pubkey)
-  console.log(key.exportKey())
+  // console.log(key.exportKey())
 
   // prefix message for plain msg
   let messageBuffer = Buffer.from(msg)
@@ -47,4 +78,75 @@ export async function verifyUniPassSig(
   )
 
   return ret
+}
+
+async function checkPubkeyOnChain(
+  username: string,
+  keyType: string,
+  pubkey: string,
+): Promise<boolean> {
+  const { local_keys } = await fetchUserInfoOnChain(username)
+
+  console.log('local_keys', local_keys)
+
+  const mapLocalPubkey = (x: Pubkey) => {
+    let keyType = ''
+    let keyValue = ''
+    if (x.rsa_pubkey) {
+      keyType = 'RsaPubkey'
+      const { e, n } = x.rsa_pubkey
+      const nA = Buffer.from(n.replace('0x', ''), 'hex')
+      const eA = Buffer.alloc(4)
+      eA.writeUInt32BE(e, 0)
+      keyValue = '0x' + Buffer.concat([eA, nA]).toString('hex')
+    } else if (x.secp256k1) {
+      keyType = 'Secp256k1Pubkey'
+      keyValue = x.secp256k1
+    }
+    return { keyType, keyValue }
+  }
+
+  const keyExist = local_keys
+    .map(mapLocalPubkey)
+    .find((x) => x.keyType === keyType && x.keyValue === pubkey)
+
+  return !!keyExist
+}
+
+async function fetchUserInfoOnChain(username: string) {
+  const usernameHash = sha256(username)
+
+  const req = {
+    id: 2,
+    jsonrpc: '2.0',
+    method: 'get_up_pending_user_info',
+    params: [usernameHash],
+  }
+
+  const resp = await axios.post(AGGREGATOR_URL, req)
+  if (resp.status !== 200) {
+    throw new Error(`aggregator request failed with HTTP code ${resp.status}`)
+  }
+  if (resp.data.error !== undefined) {
+    throw new Error(
+      `aggregator request rpc failed with error: ${JSON.stringify(
+        resp.data.error,
+      )}`,
+    )
+  }
+  return resp.data.result[0] as UserInfo
+}
+
+
+
+export function pubkeyToLockArgs(pubkey: string): string {
+  return utils.ckbHash(hexToBytes(pubkey).buffer).serializeJson().slice(0, 42);
+}
+
+export function hexToBytes(hex: HexString): Uint8Array {
+  hex = hex.startsWith('0x') ? hex.slice(2) : hex;
+  const match = hex.match(/.{1,2}/g);
+  if (!match) throw new Error(`cannot parse ${hex} into Uint8Array`);
+
+  return Uint8Array.from(match.map((byte) => parseInt(byte, 16)));
 }
--- src/verify.ts
+++ src/verify.ts
@@ -1,45 +1,36 @@
 import { config, helpers } from "@ckb-lumos/lumos";
-import { LINA_CONFIG, AGGRON4_CONFIG } from './config';
-import { UPAuthResponse } from './interface';
-import { verifyUniPassSig } from './helper';
+import { LINA_CONFIG, AGGRON4_CONFIG } from "./config";
+import { UPAuthResponse } from "./interface";
+import { verifyUniPassSig, sha256 } from "./helper";
 
-
-// compare hash(pubkey) with expected_ckb_addr 
-export function verify(payload: { 
+// compare hash(pubkey) with expected_ckb_addr
+export async function verify(payload: {
   expectedCkbAddr: string; // from event of user -NFT-> Nervape
-  sig: string; 
+  username: string;
+  sig: string;
   msg: string;
-}): boolean {
-
-  const res = JSON.parse(payload.sig) as UPAuthResponse
-  // verify msg
+}): Promise<boolean> {
   try {
-    verifyUniPassSig(
-      payload.msg,
-      res,
-    )
+    const res = JSON.parse(payload.sig) as UPAuthResponse;
+    const ret = await verifyUniPassSig(payload.username, payload.msg, res);
+
+    if (!ret) {
+      return false;
+    }
+
+    // verify addr
+    if (payload.expectedCkbAddr.startsWith("ckb")) {
+      config.initializeConfig(LINA_CONFIG);
+    } else if (payload.expectedCkbAddr.startsWith("ckt")) {
+      config.initializeConfig(AGGRON4_CONFIG);
+    }
+
+    const script = helpers.addressToScript(payload.expectedCkbAddr);
+
+    const usernameHash = sha256(payload.username);
+
+    return script.args === usernameHash.slice(0, 42);
   } catch {
     return false;
   }
-
-  console.log('verifyUniPassSig success')
-
-  // verify addr
-  if (payload.expectedCkbAddr.startsWith("ckb")) {
-    config.initializeConfig(LINA_CONFIG);
-  } else if (payload.expectedCkbAddr.startsWith("ckt")) {
-    config.initializeConfig(AGGRON4_CONFIG);
-  }
-  const script = helpers.addressToScript(payload.expectedCkbAddr)
-  console.log(script)
-
-  // const pubKey = getPubkeyFromUPKey(res.pubkey).exportKey();
-
-  // const expectedArgs = hd.key.publicKeyToBlake160(pubKey);
-
-  // console.log(script.args);
-  // console.log(expectedArgs);
-
-  return true;
-};
-
+}
